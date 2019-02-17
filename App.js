f68041865b345a54ab559134246e318e44fa3190
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-02-17 13:50:13 +0530

    working with text input

--- App.js
+++ App.js
@@ -8,33 +8,50 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View , TextInput} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+ 
 
 export default class App extends Component {
-  render() {
+
+  constructor(){
+    super();
+    this.state = {
+      value : "Edit me!"
+    }
+
+    this.handleChangeText = this.handleChangeText.bind(this);
+ }
+
+
+  handleChangeText(newValue){
+      this.setState({
+        value: newValue
+      });
+  }
+
+ render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Hello World...!</Text>
-        {/* <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text> */}
+          <TextInput 
+            defaultValue = {this.state.value}
+            onChangeText = {this.handleChangeText}
+          />
+           
+
+          <Text>You are writing {this.state.value}</Text>
       </View>
+
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flex: 1
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
